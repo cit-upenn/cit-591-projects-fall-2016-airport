43cd639691dd00d2ee46ac0d6e704450a1af73fa
bsokas <bsokas@seas.upenn.edu>
2016-12-13 11:17:10 -0500

    Added values for hour, min, and AM/PM. Code now works to use the time.

--- src/GUI.java
+++ src/GUI.java
@@ -152,17 +152,17 @@ public class GUI extends Application {
 		
 		HBox enterTime = new HBox();
 		
-		final ComboBox flightHour = new ComboBox();
+		final ComboBox<String>flightHour = new ComboBox();
 		flightHour.getItems().addAll("Hr","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12") ;
 		flightHour.getSelectionModel().selectFirst();
 		
-		final ComboBox flightMin = new ComboBox();
+		final ComboBox<String> flightMin = new ComboBox();
 		flightMin.getItems().addAll("Min", "00","01","02","03","04","05","06","07","08","09", "11","12","13","14","15","16","17", "18", "19", "20",
 				"21","22","23","24","25","26","27","28","29", "30","31","32","33","34","35","36","37","38","39", "40","41","42","43","44","45",
 				"46", "47", "48", "49", "50","51","52","53","54","55","56","57","58","59") ;
 		flightMin.getSelectionModel().selectFirst();
 		
-		final ComboBox flightAMPM = new ComboBox();
+		final ComboBox<String> flightAMPM = new ComboBox();
 		flightAMPM.getItems().addAll("AM", "PM") ;
 		flightAMPM.getSelectionModel().selectFirst();
 		
@@ -180,11 +180,14 @@ public class GUI extends Application {
 			int day = date.getDayOfMonth();
 			int year = date.getYear();
 			int dayOfWeek = date.getDayOfWeek().getValue();
-			int hour = (int) flightHour.getValue();
-			int min = (int) flightHour.getValue();
-			String amPM = (String) flightHour.getValue();
+			int hour = Integer.parseInt(flightHour.getValue());
+			int min = Integer.parseInt(flightMin.getValue());
+			if(flightAMPM.getValue() == "PM"){
+				hour = hour + 12;
+			}
+			int time = (hour * 100) + min ;
 			
-			Database db = new Database(dayOfWeek, month, 1244, day);
+			Database db = new Database(dayOfWeek, month, time, day);
 
 			
 			if(radioDepart.isSelected()){
