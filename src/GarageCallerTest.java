b9776727d1fd4b252480944013188cea961e0768
Veronika Alex <veronika.tess@gmail.com>
2016-12-18 19:29:08 -0500

    unused updated

--- src/GarageCallerTest.java
+++ src/GarageCallerTest.java
@@ -2,13 +2,30 @@ package src;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.junit.Test;
 
 public class GarageCallerTest {
 
+	GarageCaller caller;
+	
 	@Test
 	public void testRestCallerNotNull() {
 		GarageCaller gc = new GarageCaller();
 		assertNotNull("GarageCaller cannot be null", gc);
+	}
+	
+	
+	@Test
+	public void testGetGarages(){
+		ArrayList<Garage> testList = null;
+		try{
+			testList = caller.getGarages();
+		} catch(Exception e){
+			System.out.println("Test fails");
+		}
+		assertNotNull(testList);
 	}
 }
