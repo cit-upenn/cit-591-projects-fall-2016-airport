5830cf36952906b93b6b8f72260a00baa2a93235
Huong Vu <huongvu@Huongs-MacBook-Pro.local>
2016-12-18 22:24:50 -0500

    add some comments

--- src/GarageCaller.java
+++ src/GarageCaller.java
@@ -3,16 +3,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import org.json.JSONException;
-
+/**
+ * This class calls the API for garage parking.
+ * @author huongvu
+ *
+ */
 public class GarageCaller extends RestCaller {
 	
 	private static final String link = "https://api.phila.gov/airport-parking/v1/";
 	private static final String key = "garages";
 	
+	/**
+	 * This is the constructor.
+	 */
 	public GarageCaller() {
 		super();
 	}
 	
+	/**
+	 * This method returns the list of Garage objects
+	 * @return ArrayList of Garage objects
+	 * @throws JSONException
+	 * @throws IOException
+	 */
 	public ArrayList<Garage> getGarages() throws JSONException, IOException {
 		GarageJSONParser parser = new GarageJSONParser(getJSONArray(link, key));
 		GarageCollection col = new GarageCollection(parser);
--- src/TSACaller.java
+++ src/TSACaller.java
@@ -3,22 +3,42 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import org.json.JSONException;
-
+/**
+ * This class calls the API for TSA wait time.
+ * @author huongvu
+ *
+ */
 public class TSACaller extends RestCaller {
 	
 	private static final String link = "http://apps.tsa.dhs.gov/MyTSAWebService/GetTSOWaitTimes.ashx?ap=PHL&output=json";
 	private static final String key = "WaitTimes";
 	
+	/**
+	 * This is the constructor.
+	 */
 	public TSACaller() {
 		super();
 	}
 	
+	/**
+	 * This method returns the HashMap of all checkpoints with wait times.
+	 * @return HashMap of all checkpoints with wait times.
+	 * @throws JSONException
+	 * @throws IOException
+	 */
 	public HashMap<String, Integer> getAllCheckPoint() throws JSONException, IOException {
 		WaitTimeParser parser = new WaitTimeParser(getJSONArray(link, key));
 		TSAAnalyzer ana = new TSAAnalyzer(parser);
 		return ana.getTimeAtCheckpoint();
 	}
 
+	/**
+	 * This class returns the wait time for given checkpoint.
+	 * @param checkpoint you want to check wait time for
+	 * @return the wait time 
+	 * @throws JSONException
+	 * @throws IOException
+	 */
 	public int getWaitTime(String checkpoint) throws JSONException, IOException {
 		return getAllCheckPoint().get(checkpoint);
 	}
